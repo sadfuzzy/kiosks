45d080fb1a840d4cf85547a20b486880c05ce9b4
Denis Savitskiy <d.savitskiy@maximatelecom.ru>
2017-02-13 11:22:41 +0300

    Company active scope.

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,4 @@
 class Company < ApplicationRecord
-  attr_accessor :title, :active
-
   validates :title, presence: true
   scope :active, -> { where(active: true) }
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe Company, type: :model do
 
     it { is_expected.to be_valid }
   end
+
+  describe '#active' do
+    let(:company1) { Company.create title: 'Company 1', active: true }
+    let(:company2) { Company.create title: 'Company 2', active: true }
+    let(:company3) { Company.create title: 'Company 3', active: false }
+    subject { Company.active }
+
+    it { is_expected.to include company1, company2 }
+    it { is_expected.to_not include company3 }
+  end
 end
