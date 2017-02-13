5db78a9dcc7c4256a9281d0d20ecd7fc4fa01a9d
Denis Savitskiy <d.savitskiy@maximatelecom.ru>
2017-02-13 14:02:35 +0300

    Product views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,10 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.includes(:company).active.search(params[:search])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
